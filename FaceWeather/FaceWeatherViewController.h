21d120c743ed5f4358a4a1fc915d15669409113b
yasuda <yasuda@CEL-E0080.local>
2011-10-15 16:02:41 +0900

    Commit Test

---
+++ FaceWeather/FaceWeatherViewController.h
@@ -0,0 +1,21 @@
+//
+//  FaceWeatherViewController.h
+//  FaceWeather
+//
+//  Created by Toru Inoue on 11/07/28.
+//  Copyright 2011 KISSAKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FaceWeatherViewController : UIViewController {
+    
+}
+- (IBAction)droidTapped:(id)sender;
+
+- (IBAction)yokohamaTapped:(id)sender;
+- (IBAction)tan_goTapped:(id)sender;
+
+
+@end
